77032c0c4ac3a3942550e218b0520b3f5834c7f7
Oluwaremi Oluwole <polymathwole@gmail.com>
2017-05-29 20:28:11 +0100

    Add folderwatcher.js

---
+++ folderWatcher.js
@@ -0,0 +1,44 @@
+const fs=require('fs');
+const ev=require('events');
+const pa=require('path');
+
+class FileWatcher extends ev
+{
+    constructor(watchDir,processedDir)
+    {
+        super();
+        this.watchDir=watchDir;
+        this.processedDir=processedDir;
+    }
+
+    watchD()
+    {
+        fs.watchFile(this.watchDir,()=>{
+            fs.readdir(this.watchDir,(err,files)=>{
+                if(err)
+                    console.error(err.errno+' '+err.message);
+                else
+                    {
+                        for (let f of files)
+                            this.emit('process', f);
+                    }
+            })
+        });
+    }
+}
+
+const fw=new FileWatcher('C:/Users/Oluwaremi.REMI/Desktop/Bootcamp','C:/Users/Oluwaremi.REMI/Documents/Processed');
+
+fw.on('process',(file)=>{
+    let watchFile = fw.watchDir + '/' + file;
+    let processedFile = fw.processedDir + '/' + file.toLowerCase();
+
+    fs.rename(watchFile, processedFile, (err)=>{
+        if (err)
+            console.error(err.errno+' '+err.message);
+        else
+            console.log(`${watchFile} moved to ${processedFile}`);
+    });
+});
+
+fw.watchD();
